8303d7ac806278e76db5a2ede317ee735147bfad
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-06-12 21:32:33 -0700

    Add button via shadcn

---
+++ app/_components/Header.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <Button>hey</Button>
+    </div>
+  );
+};
+
+export default Header;
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./_components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <div>
+        <Header />
+      </div>
       <body className={inter.className}>{children}</body>
     </html>
   );
